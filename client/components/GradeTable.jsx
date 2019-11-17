ca8eedd4b67fc2ec0123b959d72f5bbc8f61e065
ziyaadm <ziyaadm@gmail.com>
2019-11-17 05:50:33 +0000

    added toggle

--- client/components/GradeTable.jsx
+++ client/components/GradeTable.jsx
@@ -11,7 +11,7 @@ function GradeTable(props) {
           <th scope="col">Student Name</th>
           <th scope="col">Course</th>
           <th scope="col">Grade</th>
-          <th scope="col">Operat</th>
+          <th scope="col">Operations</th>
         </tr>
       </thead>
       <tbody>
